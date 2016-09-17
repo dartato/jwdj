e8534833a4941c7303997c93caa5cc468be2d0f9
Bill Paivine <billypaivine@gmail.com>
2016-09-16 20:20:27 -0400

    Setting up Android Studio
    Creating files for app with android studio, this creates a blank android
    app.

---
+++ HackCMU16jwdj/app/src/main/java/edu/cmu/wjp/hackcmu16jwdj/MainPanel.java
@@ -0,0 +1,13 @@
+package edu.cmu.wjp.hackcmu16jwdj;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainPanel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_panel);
+    }
+}
