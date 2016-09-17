081e1b25ce3fc2015c66622870cd67bbaa47d32a
Bill Paivine <billypaivine@gmail.com>
2016-09-17 02:39:45 -0400

    More work on camera
    More work on camera, hiding preview, and sending to vision api

--- HackCMU16jwdj/app/src/main/java/edu/cmu/wjp/hackcmu16jwdj/CameraActivity.java
+++ HackCMU16jwdj/app/src/main/java/edu/cmu/wjp/hackcmu16jwdj/CameraActivity.java
@@ -2,6 +2,7 @@ package edu.cmu.wjp.hackcmu16jwdj;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
 import android.hardware.Camera;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -9,8 +10,11 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 
 import com.google.api.client.extensions.android.http.AndroidHttp;
@@ -55,10 +59,35 @@ public class CameraActivity extends Activity {
         setContentView(R.layout.activity_main_panel);
 
         appCamera = CameraUtil.getCameraInstance();
+        Camera.Parameters appCameraParameters = appCamera.getParameters();
+        appCameraParameters.setRotation(90);
+        int w =0,h =0;
+        for (Camera.Size s: appCameraParameters.getSupportedPictureSizes()) {
+            if (s.height > h) {
+                w = s.width;
+                h = s.height;
+            }
+        }
+        Log.d("onCreate", "width: "+w+" height: "+h);
+        appCameraParameters.setPictureSize(w,h);
 
         appCameraView = new CameraView(this, appCamera);
         FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
         preview.addView(appCameraView);
+        // Add a listener to the Capture button
+        Button captureButton = (Button) findViewById(R.id.button_capture);
+        captureButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // get an image from the camera
+                        appCamera.takePicture(null, null, appPicture);
+                    }
+                }
+        );
+
+        appMainImage = (ImageView) findViewById(R.id.main_image);
+
     }
 
     protected void onPause() {
@@ -71,26 +100,33 @@ public class CameraActivity extends Activity {
             appCamera = null;
         }
     }
-    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+    private Camera.PictureCallback appPicture = new Camera.PictureCallback() {
         @Override
         public void onPictureTaken(byte[] data, Camera camera) {
             File pictureFile = getCameraFile();
             if (pictureFile == null){
+                Log.d("appPicture", "Error creating media file");
                 return;
             }
 
             try {
+
                 FileOutputStream stream = new FileOutputStream(pictureFile);
                 stream.write(data);
                 stream.close();
                 uploadImage(Uri.fromFile(getCameraFile()));
+                Log.d("appPicture", "Received response from google.");
+
             } catch (FileNotFoundException e) {
+                Log.d("appPicture", "File not found: " + e.getMessage());
 
             } catch (IOException e) {
+                Log.d("appPicture", "Error accessing file: " + e.getMessage());
 
             }
         }
     };
+
 
     public void uploadImage(Uri uri) {
         if (uri != null) {
@@ -99,10 +135,11 @@ public class CameraActivity extends Activity {
                 Bitmap bitmap =
                         scaleBitmapDown(
                                 MediaStore.Images.Media.getBitmap(getContentResolver(), uri),
-                                1200);
-
-                callCloudVision(bitmap);
+                                1920);
+                alert("Image scaled, calling cloud vision");
                 appMainImage.setImageBitmap(bitmap);
+                appCameraView.setVisibility(View.GONE);
+                callCloudVision(bitmap);
 
             } catch (IOException e) {
                 //Toast.makeText(this, R.string.image_picker_error, Toast.LENGTH_LONG).show();
@@ -113,7 +150,7 @@ public class CameraActivity extends Activity {
     }
 
     public File getCameraFile() {
-        File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File dir = getFilesDir();//Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
         return new File(dir, FILE_NAME);
     }
     public Bitmap scaleBitmapDown(Bitmap bitmap, int maxDimension) {
@@ -122,6 +159,9 @@ public class CameraActivity extends Activity {
         int originalHeight = bitmap.getHeight();
         int resizedWidth = maxDimension;
         int resizedHeight = maxDimension;
+        Matrix matrix = new Matrix();
+        matrix.postRotate(90);
+        bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
 
         if (originalHeight > originalWidth) {
             resizedHeight = maxDimension;
@@ -133,7 +173,7 @@ public class CameraActivity extends Activity {
             resizedHeight = maxDimension;
             resizedWidth = maxDimension;
         }
-        return Bitmap.createScaledBitmap(bitmap, resizedWidth, resizedHeight, false);
+        return bitmap;//Bitmap.createScaledBitmap(bitmap, resizedWidth, resizedHeight, false);
     }
 
     private void callCloudVision(final Bitmap bitmap) throws IOException {
@@ -201,7 +241,7 @@ public class CameraActivity extends Activity {
             }
 
             protected void onPostExecute(String result) {
-                //Put post execute code here
+                alert(result);
             }
         }.execute();
     }
@@ -219,5 +259,32 @@ public class CameraActivity extends Activity {
         }
 
         return message;
+    }
+    private File getOutputFile(){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = getFilesDir(); //new File(Environment.getExternalStoragePublicDirectory(
+                //Environment.DIRECTORY_PICTURES), "HackCMU16jwdj");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("jwdj", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        File mediaFile = new File(mediaStorageDir.getPath() + File.separator + "tmp.jpg");
+
+        return mediaFile;
+    }
+    public void alert(String text) {
+        CharSequence alert = text;
+        Toast.makeText(getApplicationContext(), alert, Toast.LENGTH_SHORT).show();
+
     }
 }
